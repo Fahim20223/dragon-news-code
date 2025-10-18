4804238d3203294ca1422d1b440f71ab3450218f
Fahim2023 <fahimshahrier2023@gmail.com>
2025-10-19 01:06:16 +0600

    added some marquee styles and some more latest things

---
+++ src/Components/LatestNews.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const LatestNews = () => {
+  return (
+    <div className="flex items-center gap-5 bg-base-200 p-3">
+      <h2 className="text-base-100 bg-secondary px-3 py-3">Latest</h2>
+      <Marquee className="flex gap-8" pauseOnHover={true} speed={60}>
+        <p className="font-bold">
+          Match Highlights: Germany vs Spain — as it happened ! Match
+          Highlights: Germany vs Spain as...
+        </p>
+        <p className="font-bold">
+          Match Highlights: Germany vs Spain — as it happened ! Match
+          Highlights: Germany vs Spain as...
+        </p>
+        <p className="font-bold">
+          Match Highlights: Germany vs Spain — as it happened ! Match
+          Highlights: Germany vs Spain as...
+        </p>
+      </Marquee>
+    </div>
+  );
+};
+
+export default LatestNews;
