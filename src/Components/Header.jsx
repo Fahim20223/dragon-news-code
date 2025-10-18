9a179bfce5f37cdd12df50bdce027e076d1a71a2
Fahim2023 <fahimshahrier2023@gmail.com>
2025-10-19 00:39:57 +0600

    some daisyUI themes & date fomrat with data fns

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import { format } from "date-fns";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <img src={logo} alt="logo" />
+      <p className="text-accent">Journalism Without Fear or Favour</p>
+      <p>{format(new Date(), "EEEE , MMMM dd , yyyy")}</p>
+    </div>
+  );
+};
+
+export default Header;
